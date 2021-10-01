2d155e5011accc13871bc1d60fb7edcdd21e0b6a
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2021-09-30 20:38:25 -0400

    makes flash_notices work for either erb /haml

--- lib/generators/hot_glue/install_generator.rb
+++ lib/generators/hot_glue/install_generator.rb
@@ -4,13 +4,20 @@ require 'ffaker'
 module HotGlue
   class InstallGenerator < Rails::Generators::Base
     hook_for :form_builder, :as => :scaffold
+    class_option :markup, type: :string, default: "erb"
 
     source_root File.expand_path('templates', __dir__)
 
 
     def initialize(*args) #:nodoc:
       super
-      copy_file "_flash_notices.haml", "#{'spec/dummy/' if Rails.env.test?}app/views/layouts/_flash_notices.haml"
+      @markup = options['markup']
+      if @markup == "haml"
+        copy_file "haml/_flash_notices.haml", "#{'spec/dummy/' if Rails.env.test?}app/views/layouts/_flash_notices.haml"
+      elsif @markup == "erb"
+        copy_file "erb/_flash_notices.erb", "#{'spec/dummy/' if Rails.env.test?}app/views/layouts/_flash_notices.erb"
+
+      end
     end
   end
 end
