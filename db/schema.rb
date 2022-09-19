7afaa9cef9caf4353ccfd1d06272a06be291b58e
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2022-09-19 12:57:24 -0400

    more specs

--- db/schema.rb
+++ db/schema.rb
@@ -146,5 +146,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_000410) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
---
+++ dummy/spec/factories/jkl_factory.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :jkl do
+
+  end
+end
