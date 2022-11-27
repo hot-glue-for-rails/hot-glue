a7fe6d5334915b5627abf64e539199d571b316ef
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2022-11-27 13:26:57 -0500

    v0.5.4 (#23)

--- dummy/app/models/abc.rb
+++ dummy/app/models/abc.rb
@@ -2,6 +2,9 @@ class Abc < ApplicationRecord
   after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
   after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
 
+  after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
+  after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
+
   include ActionView::RecordIdentifier
   after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
   after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
