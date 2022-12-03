4f5b999c3085db5e55a4df204ddce5f96b28f2fe
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2022-12-03 12:56:24 -0500

    Jfb/2022 12 03 spec coverage fixes (#24)
    * fixes dummy file dummy/app/models/abc.rb
    
    * removing dummy/Gemfile.lock
    
    * spec coverage fixes

--- dummy/app/models/abc.rb
+++ dummy/app/models/abc.rb
@@ -1,15 +1,4 @@
 class Abc < ApplicationRecord
-  after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
-  after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
-
-  after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
-  after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
-
-  include ActionView::RecordIdentifier
-  after_update_commit lambda { broadcast_replace_to self, target: "__#{dom_id(self)}", partial: "/abcs/line" }
-  after_destroy_commit lambda { broadcast_remove_to self, target: "__#{dom_id(self)}"}
-
-
   @@table_label_plural = "Apples"
   @@table_label_singular = "Apple"
 end
