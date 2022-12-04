9021db152fe5e25ac06b6189ace9210cc9e2e172
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2022-12-04 18:43:36 -0500

    more css strategy abstraction

--- lib/generators/hot_glue/layout_strategy/base.rb
+++ lib/generators/hot_glue/layout_strategy/base.rb
@@ -9,45 +9,23 @@ module LayoutStrategy
     def button_column_style; "" ; end
     def button_style ; ""; end
     def column_headings_col_style; "" ; end
+    def column_width; ""; end
+    def column_classes_for_line_fields; ""; end
+    def column_classes_for_form_fields; ""; end
+    def column_classes_for_column_headings; ""; end
+    def col_width; 100; end
+    def container_name; ""; end
     def downnest_style ; ""; end
     def downnest_column_style ; "" ; end
-    def style_with_flex_basis(x)
-      ""
-    end
-
-    def column_width
-
-    end
-
-    def column_classes_for_line_fields
-      ""
-    end
-
-    def column_classes_for_form_fields
-      ""
-    end
-    def column_classes_for_column_headings
-      ""
-    end
-
-    def container_name
-      ""
-    end
-
-    def row_begin
-      '<div> '
-    end
-    def row_end
-      '</div> '
-    end
-
     def each_col
       return col_width if builder.columns.count == 0
       (col_width/(builder.columns.count)).to_i
     end
-
-    def col_width
-      100
-    end
+    def list_classes; ""; end
+    def row_classes; ""; end
+    def row_heading_classes; ""; end
+    def row_begin; '<div> '; end
+    def row_end ; '</div> '; end
+    def style_with_flex_basis(x); "" ; end
   end
 end
--- lib/generators/hot_glue/layout_strategy/bootstrap.rb
+++ lib/generators/hot_glue/layout_strategy/bootstrap.rb
@@ -1,18 +1,18 @@
-
-
 class LayoutStrategy::Bootstrap < LayoutStrategy::Base
-  def container_name
-    "container-fluid"
+  def button_classes
+    " " + column_classes_for_line_fields
   end
-
 
   def column_classes_for_form_fields
     "col-md-#{builder.layout_object[:columns][:size_each]}"
   end
 
+  def column_classes_for_column_headings
+    column_classes_for_line_fields
+  end
 
-  def downnest_portal_column_width(downnest)
-    "col-sm-#{ builder.layout_object[:portals][downnest][:size] }"
+  def container_name
+    "container-fluid"
   end
 
   def column_classes_for_line_fields
@@ -23,19 +23,19 @@ class LayoutStrategy::Bootstrap < LayoutStrategy::Base
     builder.layout_object[:columns][:size_each]
   end
 
+  def downnest_portal_column_width(downnest)
+    "col-sm-#{ builder.layout_object[:portals][downnest][:size] }"
+  end
+
   def row_begin
     '<div class="row"> <div class="col-md-12">'
   end
 
+  def row_classes
+    "row"
+  end
+
   def row_end
     '</div> </div>'
-  end
-
-  def column_classes_for_column_headings
-    column_classes_for_line_fields
-  end
-
-  def button_classes
-    " " + column_classes_for_line_fields
   end
 end
