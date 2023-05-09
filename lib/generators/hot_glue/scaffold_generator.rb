8b57d30d8010c8776137a450b980fcf39fb31de6
jason@heliosdev.shop <jason@heliosdev.shop>
2023-05-09 14:56:03 -0400

    fixes display of error flash notices for delete action

--- lib/generators/hot_glue/scaffold_generator.rb
+++ lib/generators/hot_glue/scaffold_generator.rb
@@ -707,9 +707,8 @@ class HotGlue::ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
       template "system_spec.rb.erb", dest_file
     end
 
-
-    if File.exist?("#{@markup}/_errors.#{@markup}")
-      File.delete("#{@markup}/_errors.#{@markup}")
+    if File.exist?("#{filepath_prefix}app/views#{namespace_with_dash}/_errors.#{@markup}")
+      File.delete("#{filepath_prefix}app/views#{namespace_with_dash}/_errors.#{@markup}")
     end
   end
 
@@ -1262,10 +1261,12 @@ class HotGlue::ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
   end
 
   def post_action_parental_updates
-    @nested_set.collect{ |data|
-      parent = data[:singular]
-      "@#{singular}.#{parent}.reload"
-    }.join("\n")
+    if @nested_set.any?
+      "\n" + @nested_set.collect{ |data|
+        parent = data[:singular]
+        "@#{singular}.#{parent}.reload"
+      }.join("\n")
+    end
   end
 
 
