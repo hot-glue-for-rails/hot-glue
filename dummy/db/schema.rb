d65ef9e693b9de15b65f3b6d5acc79f68d22efa9
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-07-04 12:46:06 -0400

    Fixes specs on main

--- dummy/db/schema.rb
+++ dummy/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_17_162618) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "abcs", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -170,8 +173,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_162618) do
   end
 
   create_table "xyzs", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "nothing_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
