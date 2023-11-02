3cc8cf1e45200596407d6f5cf803d0d851ecfc4b
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-11-02 19:39:36 -0400

    the type ahead generator can take search-by args

--- lib/generators/hot_glue/typeahead_generator.rb
+++ lib/generators/hot_glue/typeahead_generator.rb
@@ -2,6 +2,7 @@ module HotGlue
   class TypeaheadGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
     class_option :namespace, type: :string, default: nil
+    class_option :search_by, type: :string, default: nil
 
     def filepath_prefix
       # todo: inject the context
@@ -16,6 +17,15 @@ module HotGlue
 
       @plural = args.first.tableize.pluralize
       @namespace = options['namespace']
+
+      if options['search_by']
+        @search_by = options['search_by'].split(",")
+      else
+        # todo: read the fields off the table
+
+
+        raise "no search_by specified"
+      end
 
       dest_file = "#{'spec/dummy/' if $INTERNAL_SPECS}app/controllers/#{@namespace ? @namespace + "/" : ""}#{@plural }_typeahead_controller.rb"
       template "typeahead_controller.rb.erb", dest_file
