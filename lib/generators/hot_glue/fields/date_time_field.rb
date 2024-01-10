1096fa0b03e3fe746579f90530d2b50a88c537aa
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2024-01-10 11:07:45 -0500

    datetime searching

--- lib/generators/hot_glue/fields/date_time_field.rb
+++ lib/generators/hot_glue/fields/date_time_field.rb
@@ -50,14 +50,22 @@ class DateTimeField < Field
   end
 
   def search_field_output
-    ""
+    "      <div data-controller='date-range-picker' >"+
+      "\n        <%= f.select 'q[0][#{name}_match]', options_for_select([['', ''], ['is on', 'is_on'], " +
+      "\n         ['is between', 'is_between'], ['is on or after', 'is_on_or_after'], " +
+      "\n         ['is before or on', 'is_before_or_on'], ['not on', 'not_on']], @q[\'0\']['#{name}_match'] ), {} ," +
+      "\n         { class: 'form-control match', 'data-action': 'change->date-range-picker#matchSelection' } %>"+
+      "\n        <%= datetime_local_field 'q[0][#{name}_search_start]',  @q[\'0\'][:#{name}_search_start], { autocomplete: 'off', size: 40, class: 'form-control', placeholder: 'start', 'data-date-range-picker-target': 'start' } %>" +
+      "\n        <%= datetime_local_field 'q[0][#{name}_search_end]',  @q[\'0\'][:#{name}_search_end], { autocomplete: 'off', size: 40, class: 'form-control', placeholder: 'end' , 'data-date-range-picker-target': 'end' } %>" +
+      "\n      </div>"
   end
 
+
   def where_query_statement
-    ""
+    ".where(*#{name}_query)"
   end
 
   def load_all_query_statement
-    ""
+    "#{name}_query = date_query_constructor(:#{name}, @q['0'][:#{name}_match], @q['0'][:#{name}_search_start], @q['0'][:#{name}_search_end])"
   end
 end
